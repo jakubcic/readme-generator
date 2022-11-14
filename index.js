ed1d8d1faf9c3eeaafb2e59d037ac51fd204ac5e
jakub <jakub@uconn.edu>
2022-11-13 23:12:38 -0500

    fix generateMarkdown import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('utils/generateMarkdown')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 
 // TODO: Create an array of questions for user input
